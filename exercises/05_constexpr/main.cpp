f2113a29133970225e79ece802205200fcedf5b3
Abelabc <2542503181@qq.com>
2024-12-21 21:22:29 +0800

    grade3

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -6,11 +6,18 @@ constexpr unsigned long long fibonacci(int i) {
             return 0;
         case 1:
             return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+        default: {
+            unsigned long long a = 0, b = 1, c = 0;
+            for (int n = 2; n <= i; ++n) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
     }
+    return 0;
 }
-
 int main(int argc, char **argv) {
     constexpr auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
@@ -18,7 +25,7 @@ int main(int argc, char **argv) {
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
+    auto ANS_N = 90;
     auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
